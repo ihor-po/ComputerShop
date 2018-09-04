f14dbcce24c5fa8e8995b771c48ed18e1900f97c
Ihor Porotikov <sobikashi@gmail.com>
2018-09-04 22:53:16 +0300

    add field price to computer, add categorie and components

--- ComputerShop/Computer.cs
+++ ComputerShop/Computer.cs
@@ -22,6 +22,7 @@ namespace ComputerShop
     
         public int Id { get; set; }
         public string Title { get; set; }
+        public decimal Price { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ComputerItem> ComputerItem { get; set; }
